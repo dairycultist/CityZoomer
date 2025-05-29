44141c957ad5493ddd4213ec06fe9f0a1a49a92d
BlueBunnex <75032462+BlueBunnex@users.noreply.github.com>
2025-05-29 00:00:21 -0400

    Checkpoint compass 2

--- checkpoint_compass.gd
+++ checkpoint_compass.gd
@@ -3,7 +3,8 @@ extends MeshInstance3D
 @export var player_camera : Node3D
 @export var target : Node3D
 
-func _process(delta: float) -> void:
+func _process(_delta: float) -> void:
+	
 	look_at_from_position(target.global_position, player_camera.global_position)
 	position = Vector3(0, 0, -2)
 	rotation.y -= player_camera.global_rotation.y
