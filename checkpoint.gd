ed2234914b867e18cf7ab758fb3f093b2dc27f42
BlueBunnex <75032462+BlueBunnex@users.noreply.github.com>
2025-05-30 20:20:13 -0400

    Death barrier functionality

--- checkpoint.gd
+++ checkpoint.gd
@@ -4,4 +4,6 @@ func _ready() -> void:
 	self.body_entered.connect(_test_for_player)
 
 func _test_for_player(_area: Node3D):
+	
+	# remember to check that it is the player entering and not something else
 	print("test")
