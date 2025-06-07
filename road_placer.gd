176ead09ee5d7c550f7a8fe0d5980cb3a44a0be9
BlueBunnex <75032462+BlueBunnex@users.noreply.github.com>
2025-06-06 20:42:25 -0400

    Road placement 2

--- road_placer.gd
+++ road_placer.gd
@@ -1,6 +1,8 @@
 extends StaticBody3D
 
 const ROAD_STRAIGHT = preload("res://things/road/road_segment_straight.tscn")
+const ROAD_X = preload("res://things/road/road_segment_x.tscn")
+const ROAD_END = preload("res://things/road/road_segment_end.tscn")
 
 func _ready() -> void:
 	
@@ -21,6 +23,7 @@ func _ready() -> void:
 		var road = ROAD_STRAIGHT.instantiate()
 		add_child(road)
 		road.position = Vector3(x * 16, 0, z * 16)
+		road.rotation.y = direction * PI / 2
 		
 		x += sin(direction * PI / 2)
 		z += cos(direction * PI / 2)
@@ -30,12 +33,20 @@ func _ready() -> void:
 		# change direction
 		if road_length == 0:
 			
+			# place intersection
+			road = ROAD_X.instantiate()
+			add_child(road)
+			road.position = Vector3(x * 16, 0, z * 16)
+			
 			road_length = random.randi_range(3, 5)
 			
 			if direction == 1 or direction == -1:
 				direction = 0
 			else:
 				direction = random.randi_range(0, 1) * 2 - 1
+			
+			x += sin(direction * PI / 2)
+			z += cos(direction * PI / 2)
 
 func _checkpoint_touched(_area: Node3D):
 		
