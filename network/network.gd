ddc79460ae50c91c69f850e823e54858a47c884f
BlueBunnex <75032462+BlueBunnex@users.noreply.github.com>
2025-07-26 17:04:33 -0400

    Suss out more networking logistics 2

--- network/network.gd
+++ network/network.gd
@@ -15,14 +15,27 @@ extends Node
 
 # allows initialization of newly joined players
 # see that a player joined? send them a signal to spawn you!
+
 signal remoteclient_joined # (client_id: int)
 signal remoteclient_left # (client_id: int)
 signal data_recieved # (data: String)
+
+enum ClientType {
+	NO_CONNECTION,
+	SERVER_CLIENT,
+	REMOTE_CLIENT
+}
+
+var _client_type := ClientType.NO_CONNECTION
+var _client_id: int
 
 func start_serverclient() -> bool:
 	return false
 
 func start_remoteclient(ip, port) -> bool:
+	
+	# wait for server to respond with your client id
+	
 	return false
 
 # destroys the (connection to) TCP server
@@ -35,7 +48,8 @@ func broadcast(data: String) -> void:
 		# send to serverclient, telling them to broadcast
 		# also append an ID header to the data so that they know
 		# not to send it back to whoever sent it
-		pass
+		
+		send_to(0, "[broadcastfrom|" + str(get_client_id()) + "]" + data)
 	else:
 		# send to all remote clients
 		pass
@@ -47,10 +61,19 @@ func serverclient_broadcast_change_scene(scene: String) -> void:
 	get_tree().change_scene_to_file(scene)
 
 func get_client_id() -> int:
-	return 0
+	
+	match _client_type:
+	
+		ClientType.NO_CONNECTION:
+			return -1
+		
+		ClientType.SERVER_CLIENT:
+			return 0 # serverclient's client_id is always 0
+	
+	return _client_id
 
 func is_serverclient() -> bool:
-	return false
+	return _client_type == ClientType.SERVER_CLIENT
 
 func is_remoteclient() -> bool:
-	return false
+	return _client_type == ClientType.REMOTE_CLIENT
