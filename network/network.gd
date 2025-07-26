dde14a406568be781fef7ddcb79090253cf4eb20
BlueBunnex <75032462+BlueBunnex@users.noreply.github.com>
2025-07-26 19:03:08 -0400

    Fully rename to server/client

--- network/network.gd
+++ network/network.gd
@@ -1,8 +1,8 @@
 extends Node
 
-# ALL METHODS ON THIS GLOBAL SHOULD BE SERVER/REMOTE AGNOSTIC. THE PROGRAMMER
+# ALL METHODS ON THIS GLOBAL SHOULD BE SERVER/CLIENT AGNOSTIC. THE PROGRAMMER
 # SHOULD ONLY CARE ABOUT WHAT SIDE THEY'RE ON IF THEY SPECIFICALLY CALL
-# is_serverclient() OR is_remoteclient()
+# is_server() OR is_client()
 
 signal client_joined # (id: int)
 signal client_left # (id: int)
@@ -12,10 +12,10 @@ signal data_recieved # (data: String)
 enum ConnectionType { NONE, SERVER, CLIENT }
 
 enum MessageType {
-	SET_CLIENT_ID,
+	SET_ID,
 	CHANGE_SCENE, # get_tree().change_scene_to_file(scene)
-	PASS_ON, # remoteclient asking server to not read a message and just send it to another remoteclient
-	REQUEST_BROADCAST, # remoteclient asking server to broadcast to everyone else
+	PASS_ON, # client asking server to not read a message and just send it to another client
+	REQUEST_BROADCAST, # client asking server to broadcast to everyone else
 	ARBITRARY, # not handled automatically, just calls data_recieved signal w/ data
 	DONT_ADD_HEADER # in case you're rerouting data that already has headers
 }
@@ -23,12 +23,12 @@ enum MessageType {
 var _conn_type := ConnectionType.NONE
 var _id: int # technically not used by server since it will always be 0
 
-# server only
+# SERVER ONLY
 # the server keeps track of all the connected clients
 var _tcp_server: TCPServer
-var _tcp_server_connected_clients: Dictionary # int:StreamPeerTCP
+var _tcp_connected_clients: Dictionary # int:StreamPeerTCP
 
-# client only
+# CLIENT ONLY
 var _tcp_client: StreamPeerTCP
 
 
@@ -52,13 +52,13 @@ func _process(delta: float) -> void:
 				# assign an id to the connection
 				var id := RandomNumberGenerator.new().randi_range(0, 1000)
 				
-				while (_tcp_server_connected_clients.has(id)):
+				while (_tcp_connected_clients.has(id)):
 					id = RandomNumberGenerator.new().randi_range(0, 1000)
 				
-				_tcp_server_connected_clients.set(id, socket)
+				_tcp_connected_clients.set(id, socket)
 				
 				# send client id + what scene to load
-				send_to(MessageType.SET_CLIENT_ID, id, str(id))
+				send_to(MessageType.SET_ID, id, str(id))
 				#send_to(MessageType.CHANGE_SCENE, id, "")
 	
 		ConnectionType.CLIENT:
@@ -81,7 +81,7 @@ func start_server(port: int) -> bool:
 	var error := _tcp_server.listen(port)
 	
 	if error != Error.OK:
-		push_error("start_serverclient error " + str(error) + ": " + error_string(error))
+		push_error("start_server error " + str(error) + ": " + error_string(error))
 		return false
 	
 	_conn_type = ConnectionType.SERVER
@@ -96,11 +96,11 @@ func start_client(ip: String, port: int) -> bool:
 	# https://docs.godotengine.org/en/stable/classes/class_streampeertcp.html
 	_tcp_client = StreamPeerTCP.new()
 	
-	# start remote connection
+	# start client connection to server
 	var error := _tcp_client.connect_to_host(ip, port)
 	
 	if error != Error.OK:
-		push_error("start_remoteclient error " + str(error) + ": " + error_string(error))
+		push_error("start_client error " + str(error) + ": " + error_string(error))
 		return false
 	
 	_conn_type = ConnectionType.CLIENT
@@ -115,15 +115,15 @@ func broadcast(type: MessageType, data: String) -> void:
 			
 			var id_to_ignore := -1
 			
-			# parse out the id_to_ignore if this is a remoteclient-requested broadcast
+			# parse out the id_to_ignore if this is a client-requested broadcast
 			if data.begins_with("REQUEST_BROADCAST;"):
 				
 				var parts := data.split(";", true, 2)
 				id_to_ignore = parts[1].to_int()
 				data = parts[2]
 			
-			# send to all connected remoteclients
-			for id in _tcp_server_connected_clients.keys():
+			# send to all connected clients
+			for id in _tcp_connected_clients.keys():
 				if id != id_to_ignore:
 					send_to(type, id, data)
 		
@@ -139,7 +139,7 @@ func send_to(type: MessageType, client_id: int, data: String):
 		
 		ConnectionType.SERVER:
 			
-			var socket = _tcp_server_connected_clients.get(client_id)
+			var socket = _tcp_connected_clients.get(client_id)
 			if socket != null:
 				socket.put_data(data.to_ascii_buffer())
 		
@@ -155,8 +155,8 @@ func add_header(type: MessageType, data: String) -> String:
 	
 	match type:
 		
-		MessageType.SET_CLIENT_ID:
-			return "SET_CLIENT_ID;" + data
+		MessageType.SET_ID:
+			return "SET_ID;" + data
 		
 		MessageType.CHANGE_SCENE:
 			return "CHANGE_SCENE;" + data
@@ -186,7 +186,7 @@ func stop() -> void:
 func get_id() -> int:
 	match _conn_type:
 		ConnectionType.NONE:   return -1
-		ConnectionType.SERVER: return 0 # serverclient's _id is always 0
+		ConnectionType.SERVER: return 0 # server's _id is always 0
 		_:                     return _id
 
 func is_server() -> bool:
