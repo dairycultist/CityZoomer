512941fdd8f926b9d385db7e0631bc88e898b429
BlueBunnex <75032462+BlueBunnex@users.noreply.github.com>
2025-08-18 17:47:51 -0400

    Rooms connect at arbitrary yaws

--- level/level_placer.gd
+++ level/level_placer.gd
@@ -7,7 +7,7 @@ var random = RandomNumberGenerator.new()
 
 func _ready() -> void:
 	
-	place_room(connector_rooms[0], Node3D.new())
+	place_room(connector_rooms[0], Node3D.new(), 3)
 
 # rooms have children in group "Door" for connecting together (Z+ is outwards)
 func get_doors(room: Node3D) -> Array[Node3D]:
@@ -23,13 +23,23 @@ func get_doors(room: Node3D) -> Array[Node3D]:
 
 # TODO my_collider.get_aabb() to ensure they don't intersect
 
-func place_room(room_prefab: PackedScene, door: Node3D) -> void:
+func place_room(room_prefab: PackedScene, from_door: Node3D, max_depth: int) -> void:
+	
+	if (max_depth <= 0):
+		return
 	
 	# place the room such that it is connected with the door
 	var room = room_prefab.instantiate()
 	add_child(room)
-	room.position = door.global_position - get_doors(room)[0].position
+	
+	var doors := get_doors(room)
+	var to_door: Node3D = doors.pop_front()
+	
+	room.rotation.y = from_door.rotation.y - to_door.rotation.y + PI
+	room.position = from_door.global_position + (to_door.position.rotated(Vector3.UP, -room.rotation.y))
 	
 	# place rooms at all other doors
 	# 	right now just connects a random possible room to a room (a room
 	# 	cannot specify which rooms are allowed to be placed next to it)
+	while not doors.is_empty():
+		place_room(connector_rooms[1], doors.pop_front(), max_depth - 1)
