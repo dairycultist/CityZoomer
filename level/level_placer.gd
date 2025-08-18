eaaec35564b67306f26aadb4469f12ba21f14d46
BlueBunnex <75032462+BlueBunnex@users.noreply.github.com>
2025-08-18 16:43:33 -0400

    Prepare dynamic room placement system

--- level/level_placer.gd
+++ level/level_placer.gd
@@ -1,76 +1,24 @@
 extends StaticBody3D
 
-const ROAD_STRAIGHT = preload("./road/road_segment_straight.tscn")
-const ROAD_X = preload("./road/road_segment_x.tscn")
-const ROAD_END = preload("./road/road_segment_end.tscn")
+# have empty Node3Ds in group "Door" for connecting together with
+# Z+ arrow (BLUE) pointing out of the door
 
-const T_CORRIDOR = preload("./t_corridor.tscn")
+# my_collider.get_aabb() to ensure they don't intersect
+
+# right now just connects a random possible room to a room (a room cannot
+# specify which rooms are allowed to be placed next to it)
+@export var connector_rooms: Array[PackedScene]
+@export var dead_end_rooms: Array[PackedScene]
 
 var random = RandomNumberGenerator.new()
 
-var x = 0
-var z = 0
-var direction = 0 # -1, 0, 1
-
 func _ready() -> void:
 	
-	place_random()
+	place_room(connector_rooms[0])
 
-# "PlacementModule" system (have an entrance and an exit, and are guaranteed to
-# never intersect with others. maybe check AABB before instantiating?)
-func place_random():
+func place_room(room: PackedScene) -> void:
 	
-	for xb in range(0, 20):
-		for zb in range(0, 20):
+	var t = room.instantiate()
 			
-			var t = T_CORRIDOR.instantiate()
-			
-			add_child(t)
-			t.position = Vector3(xb * 12, 0, zb * 12)
-	
-	#place_curvy_road(20)
-
-func place_curvy_road(length: int):
-	
-	var road_length = random.randi_range(3, 5)
-	
-	for i in range(0, length):
-		
-		# build road
-		var road = ROAD_STRAIGHT.instantiate()
-		add_child(road)
-		road.position = Vector3(x * 16, 0, z * 16)
-		road.rotation.y = direction * PI / 2
-		
-		x += sin(direction * PI / 2)
-		z += cos(direction * PI / 2)
-		
-		road_length -= 1
-		
-		# change direction
-		if road_length == 0:
-			
-			road_length = random.randi_range(3, 5)
-			
-			# place intersection
-			road = ROAD_X.instantiate()
-			add_child(road)
-			road.position = Vector3(x * 16, 0, z * 16)
-			
-			road = ROAD_END.instantiate()
-			add_child(road)
-			road.position = Vector3((x + sin(direction * PI / 2)) * 16, 0, (z + cos(direction * PI / 2)) * 16)
-			road.rotation.y = direction * PI / 2
-			
-			if direction == 1 or direction == -1:
-				direction = 0
-			else:
-				direction = random.randi_range(0, 1) * 2 - 1
-			
-			road = ROAD_END.instantiate()
-			add_child(road)
-			road.position = Vector3((x - sin(direction * PI / 2)) * 16, 0, (z - cos(direction * PI / 2)) * 16)
-			road.rotation.y = direction * PI / 2 + PI
-			
-			x += sin(direction * PI / 2)
-			z += cos(direction * PI / 2)
+	add_child(t)
+	t.position = Vector3(0, 0, 0)
