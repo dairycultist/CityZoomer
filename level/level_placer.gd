ddd390146612091425b43608950116a09e93fabf
BlueBunnex <75032462+BlueBunnex@users.noreply.github.com>
2025-08-18 17:28:55 -0400

    Place a single room

--- level/level_placer.gd
+++ level/level_placer.gd
@@ -7,18 +7,28 @@ var random = RandomNumberGenerator.new()
 
 func _ready() -> void:
 	
-	place_room(connector_rooms[0], Transform3D(Basis(), Vector3.ZERO))
+	place_room(connector_rooms[0], Node3D.new())
 
 # rooms have children in group "Door" for connecting together (Z+ is outwards)
+func get_doors(room: Node3D) -> Array[Node3D]:
+	
+	var doors: Array[Node3D] = []
+	
+	for child in room.get_children():
+		
+		if child.is_in_group("Door"):
+			doors.append(child)
+	
+	return doors
 
 # TODO my_collider.get_aabb() to ensure they don't intersect
 
-func place_room(room: PackedScene, door: Transform3D) -> void:
+func place_room(room_prefab: PackedScene, door: Node3D) -> void:
 	
 	# place the room such that it is connected with the door
-	var t = room.instantiate()
-	add_child(t)
-	t.position = Vector3(0, 0, 0)
+	var room = room_prefab.instantiate()
+	add_child(room)
+	room.position = door.global_position - get_doors(room)[0].position
 	
 	# place rooms at all other doors
 	# 	right now just connects a random possible room to a room (a room
