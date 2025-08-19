6a934967697b84b45f4f0d8e26491431e71f883e
BlueBunnex <75032462+BlueBunnex@users.noreply.github.com>
2025-08-18 21:09:01 -0400

    Prevent room intersection implementation

--- level/level_placer.gd
+++ level/level_placer.gd
@@ -29,6 +29,35 @@ func place_door_stopper(door: Node3D) -> void:
 	stopper.global_position = door.global_position
 	stopper.global_rotation = door.global_rotation
 
+# gets AABB in global space
+func get_room_aabb(room: Node3D) -> AABB:
+	
+	var shape = room.shape as ConcavePolygonShape3D
+	var aabb = AABB()
+	for face in shape.get_faces():
+		aabb = aabb.expand(face.rotated(Vector3.UP, room.rotation.y))
+	
+	aabb.position += room.position
+	
+	return aabb
+
+func room_causes_intersection(room: Node3D) -> bool:
+	
+	# get room's bounding box (shrunken slightly due to precision)
+	var aabb := get_room_aabb(room)
+	
+	aabb.size -= Vector3(0.2, 0.2, 0.2)
+	aabb.position += Vector3(0.1, 0.1, 0.1)
+	
+	# compare against all other rooms
+	for other_room in get_children():
+		
+		if room != other_room:
+			if aabb.intersects(get_room_aabb(other_room)):
+				return true
+	
+	return false
+
 func place_random_room(from_door: Node3D, max_depth: int) -> void:
 	
 	if (max_depth <= 0):
@@ -36,13 +65,13 @@ func place_random_room(from_door: Node3D, max_depth: int) -> void:
 		return
 	
 	# attempt to place a random room
-	var room_pool = rooms.duplicate()
+	var room_pool := rooms.duplicate()
 	room_pool.shuffle()
 	
 	while not room_pool.is_empty():
 	
 		# place the room such that it is connected with from_door
-		var room = room_pool.pop_back().instantiate()
+		var room: Node3D = room_pool.pop_back().instantiate()
 		add_child(room)
 		
 		var doors := get_doors(room)
@@ -51,10 +80,13 @@ func place_random_room(from_door: Node3D, max_depth: int) -> void:
 		room.global_rotation.y = from_door.global_rotation.y - to_door.global_rotation.y + PI
 		room.global_position = from_door.global_position - (to_door.position.rotated(Vector3.UP, room.rotation.y))
 		
-		#if intersecting with other rooms, delete and try again
-		if (false):
-			#my_collider.get_aabb()
-			pass
+		# if intersecting with other rooms, delete and try again
+		# (room root node should be its collider)
+		if (room_causes_intersection(room)):
+			
+			remove_child(room)
+			room.queue_free()
+			
 		else:
 			# otherwise, place rooms at all open doors of this room
 			# 	right now just connects a random possible room to a room (a room
