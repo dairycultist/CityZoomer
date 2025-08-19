520c5a55ca1828cfc02cbae782dfcc727c708cec
BlueBunnex <75032462+BlueBunnex@users.noreply.github.com>
2025-08-18 20:49:29 -0400

    Prep prevent intersections 3

--- level/level_placer.gd
+++ level/level_placer.gd
@@ -8,7 +8,7 @@ var random = RandomNumberGenerator.new()
 func _ready() -> void:
 	
 	# TODO handle spawn room differently since it isn't placed off of a door
-	place_random_room(Node3D.new(), 2)
+	place_random_room(Node3D.new(), 3)
 
 # rooms have children in group "Door" for connecting together (Z+ is outwards)
 func get_doors(room: Node3D) -> Array[Node3D]:
@@ -35,24 +35,36 @@ func place_random_room(from_door: Node3D, max_depth: int) -> void:
 		place_door_stopper(from_door)
 		return
 	
-	# TODO attempt to place a random door, then check my_collider.get_aabb()
-	# to ensure no intersections (if fails, delete and place a door stopper)
+	# attempt to place a random room
+	var room_pool = rooms.duplicate()
+	room_pool.shuffle()
 	
-	# place the room such that it is connected with from_door
-	var room = rooms[random.randi() % rooms.size()].instantiate()
-	add_child(room)
+	while not room_pool.is_empty():
 	
-	var doors := get_doors(room)
-	var to_door: Node3D = doors.pop_back()
+		# place the room such that it is connected with from_door
+		var room = room_pool.pop_back().instantiate()
+		add_child(room)
+		
+		var doors := get_doors(room)
+		var to_door: Node3D = doors.pop_back()
+		
+		room.global_rotation.y = from_door.global_rotation.y - to_door.global_rotation.y + PI
+		room.global_position = from_door.global_position - (to_door.position.rotated(Vector3.UP, room.rotation.y))
+		
+		#if intersecting with other rooms, delete and try again
+		if (false):
+			#my_collider.get_aabb()
+			pass
+		else:
+			# otherwise, place rooms at all open doors of this room
+			# 	right now just connects a random possible room to a room (a room
+			# 	cannot specify which rooms are allowed to be placed next to it)
+			while not doors.is_empty():
+				place_random_room(
+					doors.pop_back(),
+					max_depth - 1
+				)
+			return
 	
-	room.global_rotation.y = from_door.global_rotation.y - to_door.global_rotation.y + PI
-	room.global_position = from_door.global_position - (to_door.position.rotated(Vector3.UP, room.rotation.y))
-	
-	# place rooms at all other doors
-	# 	right now just connects a random possible room to a room (a room
-	# 	cannot specify which rooms are allowed to be placed next to it)
-	while not doors.is_empty():
-		place_random_room(
-			doors.pop_back(),
-			max_depth - 1
-		)
+	# if there are no more possible rooms, place a door stopper
+	place_door_stopper(from_door)
