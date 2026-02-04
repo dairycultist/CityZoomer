98d87a8508ac9787922acfc404d89b87a2aa62c0
BlueBunnex <75032462+BlueBunnex@users.noreply.github.com>
2026-02-04 18:58:44 -0500

    Remove b-hopping since I don't know how to tune it

--- player/player.gd
+++ player/player.gd
@@ -7,14 +7,11 @@ var camera_pitch := 0.0
 @export var max_camera_distance: float = 3.0
 
 @export_group("Movement")
-@export var ground_accel: float    = 25
-@export var air_accel: float       = 25
-@export var max_velocity: float    = 5
-@export var ground_friction: float = 8
-@export var jump_speed: float      = 8
-## Lower values make it easier to gain speed. Higher values make it easier to
-## change direction in the air while maintaining speed.
-@export_range(0.0, 0.1, 0.001, "or_greater") var forward_coherence: float = 0.05
+@export var accel: float      = 25
+@export var max_speed: float  = 5
+@export var drag: float       = 8
+@export var jump_speed: float = 8
+@export var gravity: float    = 25
 
 @export_group("IK")
 @export var left_hand: Node3D
@@ -44,11 +41,21 @@ func _process(delta: float) -> void:
 	var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 	
 	# gravity
-	velocity.y -= 25.0 * delta
+	velocity.y -= gravity * delta
 	
 	# moving
 	if direction:
-		velocity = accelerate(delta, ground_accel if is_on_floor() else air_accel, direction, velocity)
+		
+		velocity += direction * accel * delta
+		
+		var vel2d := Vector2(velocity.x, velocity.z)
+
+		if (vel2d.length() > max_speed):
+			
+			vel2d = vel2d.normalized() * max_speed
+			
+			velocity.x = vel2d.x
+			velocity.z = vel2d.y
 	
 	if is_on_floor():
 		
@@ -59,10 +66,10 @@ func _process(delta: float) -> void:
 			$HopSound.pitch_scale = randf_range(0.9, 1.1)
 			$HopSound.play()
 		
-		else:
+		elif not direction:
 			
 			# drag
-			velocity = lerp(velocity, Vector3.ZERO, ground_friction * delta)
+			velocity = lerp(velocity, Vector3.ZERO, drag * delta)
 	
 	move_and_slide()
 	
@@ -76,17 +83,6 @@ func _process(delta: float) -> void:
 	else:
 		$CameraAnchor/Camera3D.global_position = $CameraAnchor.global_position + max_camera_distance * $CameraAnchor.global_transform.basis.z
 
-# https://adrianb.io/2015/02/14/bunnyhop.html
-func accelerate(delta: float, accel: float, inputDirection: Vector3, prevVelocity: Vector3) -> Vector3:
-	
-	var projVel := prevVelocity.dot(inputDirection)
-	var accelVel := accel * delta
-
-	if (projVel + accelVel > max_velocity):
-		accelVel = max_velocity - projVel
-
-	return prevVelocity + inputDirection * accelVel
-
 func _input(event):
 	
 	if event.is_action_pressed("pause"):
@@ -98,16 +94,15 @@ func _input(event):
 	
 	if event is InputEventMouseMotion and Input.mouse_mode == Input.MOUSE_MODE_CAPTURED:
 		
-		rotation.y += deg_to_rad(-event.relative.x * mouse_sensitivity)
+		var rotation_angle := deg_to_rad(-event.relative.x * mouse_sensitivity)
+		
+		rotation.y += rotation_angle
 		
 		camera_pitch = clampf(camera_pitch - event.relative.y * mouse_sensitivity, -90, 90)
 		
 		$CameraAnchor.rotation.x = deg_to_rad(camera_pitch)
 		
-		# forward coherence (try to align velocity w/ facing direction when turning)
-		var forward          := Vector2(-transform.basis.z.x, -transform.basis.z.z).normalized()
-		var velocity_2d      := Vector2(velocity.x, velocity.z)
-		var cohered_velocity := forward * forward.dot(velocity_2d)
-		var fac              := minf(velocity_2d.length() * forward_coherence * abs(event.relative.x) * mouse_sensitivity, 1.0)
-		velocity.x = lerp(velocity.x, cohered_velocity.x, fac)
-		velocity.z = lerp(velocity.z, cohered_velocity.y, fac)
+		# rotate velocity with camera rotation
+		var rotated := Vector2(velocity.x, velocity.z).rotated(-rotation_angle)
+		velocity.x = rotated.x
+		velocity.z = rotated.y
