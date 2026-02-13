07d1c64f0776ecebb36a43fa84d8f9f4d1668e3a
BlueBunnex <75032462+BlueBunnex@users.noreply.github.com>
2026-02-13 13:36:30 -0500

    Player rifle animation

--- player/local_player.gd
+++ player/local_player.gd
@@ -24,6 +24,13 @@ func _process(delta: float) -> void:
 	else:
 		if Input.is_action_just_pressed("fire"):
 			_shoot()
+	
+	# rifle animation
+	$CameraAnchor/Rifle.rotation.x = lerp($CameraAnchor/Rifle.rotation.x, 0.0, 10.0 * delta)
+	$CameraAnchor/Rifle.rotation.y = lerp($CameraAnchor/Rifle.rotation.y, -PI, 10.0 * delta)
+	
+	# TODO make this not hardcoded
+	$CameraAnchor/Rifle.position.y = -0.234 + sin(Time.get_ticks_msec() * 0.02) * 0.01 * (velocity.length() / max_speed)
 
 func _input(event):
 	
@@ -37,3 +44,6 @@ func _input(event):
 	elif event is InputEventMouseMotion and Input.mouse_mode == Input.MOUSE_MODE_CAPTURED:
 		
 		_change_look(-event.relative.x * mouse_sensitivity, event.relative.y * mouse_sensitivity)
+		
+		$CameraAnchor/Rifle.rotation.x += event.relative.y * 0.001
+		$CameraAnchor/Rifle.rotation.y -= event.relative.x * 0.001
